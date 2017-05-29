65361dd1a1c8e8d6a11ee1c1c9e2ba6861def1d5
Slavko Udarnikov <uslavko@protonmail.com>
2017-05-30 02:31:49 +0300

    A working prototype of the local range filter. However, it gains the noise on CT areas

--- savebook.cpp
+++ savebook.cpp
@@ -34,7 +34,7 @@ static void help_normalize()
   cout << "Usage: savebook <operation> [arguments...]\n" << endl;
 }
 
-static void normalize_value( InputArray src, OutputArray dst, int ksize );
+static void normalize_value( InputOutputArray hsv, int ksize );
 
 int normalize( int argc, char** argv )
 {
@@ -44,50 +44,94 @@ int normalize( int argc, char** argv )
   }
 
   char* filename = argv[1];
-  Mat _src = imread( filename, IMREAD_COLOR );
+  Mat src = imread( filename, IMREAD_COLOR );
 
-  if ( _src.empty() ) {
-	cout << "Image is empty.\n";
+  if ( src.empty() ) {
+	cerr << "Image is empty.\n";
 	return 2;
   }
 
-  Mat src;
-  _src.convertTo( src, CV_32F );
+  Mat img;
 
-  Mat _dst;
-  normalize_value( src, _dst, 12 );
+  switch ( src.depth() ) {
+  case CV_8S:
+	src.convertTo( img, CV_32F, 1.0D/255, 0.5 );
+	break;
+  case CV_8U:
+	src.convertTo( img, CV_32F, 1.0D/255, 0.0 );
+	break;
+  case CV_16S:
+	src.convertTo( img, CV_32F, 1.0D/65535, 0.5 );
+	break;
+  case CV_16U:
+	src.convertTo( img, CV_32F, 1.0D/65535, 0.0 );
+	break;
+  default:
+	cerr << "Unsupported depth!\n";
+	return 4;
+  }
+	
+  cvtColor( img, img, COLOR_BGR2HSV );
+
+  normalize_value( img, 12 );
+
+  cvtColor( img, img, COLOR_HSV2BGR );
 
   Mat dst;
-  _dst.convertTo( dst, CV_8U );
+
+  switch ( src.depth() ) {
+  case CV_8S:
+	img.convertTo( dst, CV_8S, 255, -128 );
+	break;
+  case CV_8U:
+	img.convertTo( dst, CV_32F, 255, 0 );
+	break;
+  case CV_16S:
+	img.convertTo( dst, CV_32F, 65535, -32768 );
+	break;
+  case CV_16U:
+	img.convertTo( dst, CV_32F, 65535, 0 );
+	break;
+  default:
+	cerr << "Unsupported depth!\n";
+	return 4;
+  }
 
   if ( imwrite( "out.ppm", dst ) ) {
 	return 0;
   } else {
-	return 3;
+	cerr << "Error writing output image!\n";
+	return 4;
   }
 }
 
-static void normalize_value( InputArray src, OutputArray dst, int ksize )
+static void normalize_value( InputOutputArray hsv, int ksize )
 {
-  Mat hsv;
-  cvtColor( src, hsv, COLOR_BGR2HSV );
-
-  Mat src_v;
-  extractChannel( hsv, src_v, 2 );
+  Mat v;
+  extractChannel( hsv, v, 2 );
   
   Mat w = getStructuringElement( MORPH_RECT, Size(ksize, ksize) );
   Mat k = Mat::ones( ksize, ksize, CV_32F ) / (float)( ksize * ksize );
   
   Mat maxavg_v;
-  dilate( src_v, maxavg_v, w );
-  filter2D( maxavg_v, maxavg_v, -1 , k);
+  dilate( v, maxavg_v, w );
+  filter2D( maxavg_v, maxavg_v, -1 , k );
 
   Mat minavg_v;
-  erode( src_v, minavg_v, w );
-  filter2D( minavg_v, minavg_v, -1 , k);
+  erode( v, minavg_v, w );
+  filter2D( minavg_v, minavg_v, -1 , k );
 
-  insertChannel( minavg_v, hsv, 2 );
-  cvtColor( hsv, dst, COLOR_HSV2BGR );
+  for ( int i = 0; i < minavg_v.rows; i++ ) {
+    float* v_i = v.ptr<float>(i);
+    float* min_i = minavg_v.ptr<float>(i);
+    float* max_i = maxavg_v.ptr<float>(i);
+    for ( int j = 0; j < minavg_v.cols; j++ ) {
+	  float r = 1.0F / (max_i[j] - min_i[j]);
+	  v_i[j] = (v_i[j] - min_i[j]) * r;
+	}
+  }
+
+  insertChannel( v, hsv, 2 );
 }
 
 int main( int argc, char** argv )
